f6c34852341085350696e7bb0eb2f633a67d49d2
Ryan Stocker <stocker.19@osu.edu>
2012-11-16 13:00:10 -0500

    scratching last idea

--- app/models/email_log.rb
+++ app/models/email_log.rb
@@ -1,7 +1,3 @@
 class EmailLog < ActiveRecord::Base
   attr_accessible :to, :subject, :from, :body
-
-  def text_part
-    message.multipart? ? (message.text_part ? message.text_part.body.decoded : nil) : message.body.decoded
-  end
 end
