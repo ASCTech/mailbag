9d95d2f010ac88d471a067f3d28f8417754fb57e
Ryan Stocker <stocker.19@osu.edu>
2012-11-16 11:48:01 -0500

    email_log.text_part - grab text portion of email, multipart or not

--- app/models/email_log.rb
+++ app/models/email_log.rb
@@ -1,3 +1,7 @@
 class EmailLog < ActiveRecord::Base
   attr_accessible :to, :subject, :from, :body
+
+  def text_part
+    message.multipart? ? (message.text_part ? message.text_part.body.decoded : nil) : message.body.decoded
+  end
 end
