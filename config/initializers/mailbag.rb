67c60072eee5b5c92389d949ba32aedb3d2b7ded
Ryan Stocker <stocker.19@osu.edu>
2012-11-16 11:41:20 -0500

    support for multipart emails

--- config/initializers/mailbag.rb
+++ config/initializers/mailbag.rb
@@ -2,7 +2,8 @@ class MailbagLogger
   def self.delivered_email(message)
     to = message.to.is_a?(Array) ? message.to.join(',') : message.to.to_s
     from = message.from.is_a?(Array) ? message.from.join(',') : message.from.to_s
-    EmailLog.create(:to => to, :from => from, :subject => message.subject, :body => message.body.to_s)
+    text_part = message.multipart? ? (message.text_part ? message.text_part.body.decoded : nil) : message.body.decoded
+    EmailLog.create(:to => to, :from => from, :subject => message.subject, :body => text_part)
   end
 end
 
